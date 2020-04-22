e4252212beebefff685c1b33aceca815cfd022f3
Dániel Kovács <dankovacs94@gmail.com>
2020-04-22 11:45:33 +0200

    get image hashes from given URL, then build a new URL

--- SearchOutfit/src/main/java/com/searchoutfit/app/AppApplication.java
+++ SearchOutfit/src/main/java/com/searchoutfit/app/AppApplication.java
@@ -1,37 +1,13 @@
 package com.searchoutfit.app;
 
-import com.searchoutfit.app.imageprocess.aboutYouPOJO2.AboutYouPOJO2;
 import com.searchoutfit.app.imageprocess.service.ClothService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
 public class AppApplication {
-
-
-    @Autowired
-    ClothService clothService;
-
-    public static final String apiEndpoint = "https://api-cloud.aboutyou.de/v1/products?page=";
-    List<AboutYouPOJO2> items = new ArrayList<>();
-
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
     }
-
-    @PostConstruct
-    public void init(){
-        for (int i = 1; i < 10; i++) {
-            items.add(clothService.getImagesFromURL(apiEndpoint+i+"&perPage=1000"));
-        }
-        System.out.println("asd");
-    }
-
-
 }
--- SearchOutfit/src/main/java/com/searchoutfit/app/imageprocess/service/ClothService.java
+++ SearchOutfit/src/main/java/com/searchoutfit/app/imageprocess/service/ClothService.java
@@ -7,10 +7,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+
 @Service
 public class ClothService {
 
-    public AboutYouPOJO2 getImagesFromURL(String url){
+    private List<AboutYouPOJO2> items = new ArrayList<>();
+    private List<String> imageUrls = new ArrayList<>();
+
+    private AboutYouPOJO2 getImagesFromURL(String url){
         RestTemplate restTemplate = new RestTemplate();
         HttpEntity<AboutYouPOJO2> request = new HttpEntity<>(new AboutYouPOJO2());
         ResponseEntity<AboutYouPOJO2> resp = restTemplate.exchange(url,
@@ -18,6 +26,25 @@ public class ClothService {
                 request,
                 AboutYouPOJO2.class);
         return resp.getBody();
+    }
+
+    @PostConstruct
+    private void init(){
+        for (int i = 1; i < 10; i++) {
+            items.add(getImagesFromURL("https://api-cloud.aboutyou.de/v1/products?page="+i+"&perPage=1000"));
+        }
+        getHashes();
+        System.out.println(getUrls().toString());
+    }
+
+    private void getHashes(){
+        items.forEach(x-> x.getEntities()
+                .forEach(y-> y.getImages()
+                        .forEach(z-> imageUrls.add("https://cdn.aboutstatic.com/file/images/" + z.getHash() + "?width=600&height=800&quality=100"))));
+    }
+
+    public List<String> getUrls(){
+        return imageUrls;
     }
 
 }
